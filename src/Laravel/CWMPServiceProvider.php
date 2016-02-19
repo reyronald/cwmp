b76c3c6dc69e98602628c30d24859523b505da93
Ronald Rey <reyronald@gmail.com>
2016-02-19 10:03:04 -0400

    service provider

---
+++ src/Laravel/CWMPServiceProvider.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace CWMP\Laravel;
+
+use Exception;
+use Illuminate\Support\ServiceProvider;
+use InvalidArgumentException;
+use Symfony\Component\HttpFoundation\Response;
+
+class CWMPServiceProvider extends ServiceProvider
+{
+	public function boot()
+	{
+		$this->garbageCollect();
+	}
+
+	public function register()
+	{
+		$this->prepareResources();
+	}
+
+	protected function prepareResources()
+	{
+        // Publish config
+        $config = realpath(__DIR__.'/../config/config.php');
+
+        $this->mergeConfigFrom($config, 'rrey.cwmp');
+        
+        $this->publishes([
+            $config => config_path('rrey.cwmp.php'),
+        ], 'config');
+
+        // Publish migrations
+        $migrations = realpath(__DIR__.'/../migrations');
+
+        $this->publishes([
+            $migrations => $this->app->databasePath().'/migrations',
+        ], 'migrations');
+
+	}
+}
